1abdbf31109bf76d45c4cefb48a1f20ac45912ae
mohd sharjeel <srjn0201@gmail.com>
2025-10-01 12:06:35 +0530

    first commit

---
+++ dataPipeline/src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+// # To configure the Release build (in a new build/release directory)
+// cmake --preset release
+
+// # To compile it
+// cmake --build build/release
+
+
+
+int main() {
+    std::cout << "Hello World" << std::endl;
+    return 0;
+}
